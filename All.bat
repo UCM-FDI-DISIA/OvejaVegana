b3bd934a77d9ddda128023336326de712eab8e02
CristinaMora <Crmora03@ucm.es>
2024-04-25 18:53:08 +0200

    creacion de .bat para la compilacion de motor
    copia de assets basicamente

---
+++ All.bat
@@ -0,0 +1,14 @@
+cd VeryReal\VeryRealSolucion\dependencies\cMake
+call All.bat
+cd ..
+cd ..\bin
+mkdir .\Assets
+
+cd ..\..
+cd ..\bin\ 
+
+xcopy Assets .\..\VeryReal\VeryRealSolucion\bin\Assets /E /Y
+
+cd ..\VeryReal\VeryRealSolucion\
+msbuild "VeryRealSolucion.sln" /p:configuration=Debug /p:Platform=x64 /p:PlatformToolset=v143
+msbuild "VeryRealSolucion.sln" /p:configuration=Release /p:Platform=x64 /p:PlatformToolset=v143
