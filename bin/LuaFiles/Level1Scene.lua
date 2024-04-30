0c413fe71461f98cccbcdbc992330d0a01b7e7b0
josepk01 <72452701+josepk01@users.noreply.github.com>
2024-04-30 12:07:21 +0200

    inicio creacion mapa oveja vegana

--- bin/LuaFiles/Level1Scene.lua
+++ bin/LuaFiles/Level1Scene.lua
@@ -5,33 +5,43 @@ Entities={
             {
                 name = "TransformComponent",
                 parameters = {  
-                    position = {0, 10, 0},
+                    position = {0, 0, 10},
                     rotation ={0, 0, 0},
                     scale = {1, 1, 1}                   
                 }
             },
+           
+  
+            {
+                name = "LightComponent",
+                parameters = {
+                    type = 1,
+                    diffusecolour = {1, 1, 1},
+                    shadowfardist = 25,
+                    shadowdist = 25,
+                    ineerangle = 90,
+                    outerangle = 180,
+                    nearclipdist = 0.1,
+                    shdws = true
+                }
+	        },
             {
                 name = "ColliderComponent",
-                parameters = {                
+                parameters = {
                 }
             },
             {
-                name = "RigidbodyComponent",
-                parameters = {    
-                    shapeType = SHAPES_BOX,
-                    movementType = MOVEMENT_TYPE_STATIC,
-                    size = {1, 1, 1},
-                    mask = 3,
-                    group = 2
-                }
-            },
-            {
-                name = "MeshRenderComponent",
-                parameters = {                  
-                    isstatic = true,
-                    modelname = "enemigo.mesh",
-                    entityname= "sinbad",
-                    materialname = ""
+                name = "RigidBodyComponent",
+                parameters = {
+                    shapeType = 1,
+                    movementType = 0,
+                    size = {2, 2, 2},
+                    mass = 1,
+                    friction = 0,
+                    restitution = 0,
+                    trigger = false,
+                    mask = 2,
+                    group = 1
                 }
             },
             {
@@ -41,16 +51,59 @@ Entities={
                 }
             },
             {
+                name = "PlayerInputComponent",
+                parameters = {                  
+                    
+                }
+            }              
+        }
+    }, 
+    {
+        name = "Camera",
+        components = {
+            -- {
+            --     name = "TransformComponent",
+            --     parameters = {
+            --         position = {0, 10, -20},  -- Elevada a 10 unidades y colocada detrás del origen
+            --         rotation = {0, 0, 0},  -- Mirando directamente hacia el horizonte
+            --         scale = {1, 1, 1}
+            --     }
+            -- },
+            {
+                name = "TransformComponent",
+                parameters = {  
+                    position = {0, 0, 10},
+                    rotation ={10, 90, 0},
+                    scale = {1, 1, 1}                   
+                }
+            },
+            -- {
+            --     name = "CameraComponent",
+            --     parameters = {                  
+            --         name = "anim",
+            --         color = {0.8, 0.3, 1},
+            --         alfa = 1,
+            --         offset = {100, 100, 100}
+            --     }
+            -- },
+            {
                 name = "CameraComponent",
                 parameters = {                  
                     name = "anim",
-                    color = {0.8, 0.3, 1},
-                    alfa = 1,
-                    offset = {100, 100, 100}
+                    color = {1, 0, 0},
+                    alfa = 0,
+                    offset = {0, 0, 0}
                 }
-            }                           
+            }, 
+            {
+                name = "InputComponent", -- Suponiendo que tienes un componente para manejar entradas
+                parameters = {
+                    moveSpeed = 5.0, -- Velocidad de movimiento de la cámara
+                    rotateSpeed = 0.1 -- Velocidad de rotación de la cámara
+                }
+            }
         }
-    }, 
+    },
     {  
         name = "Light",
         components = {
@@ -79,13 +132,13 @@ Entities={
         }
     }, 
     {
-        name = "Ogreman",
+        name = "Ogreman5",
         components = {
             {
                 name = "TransformComponent",
                 parameters = {  
                     a = 0,
-                    position = {0, 0, 20},
+                    position = {10, 0, -10},
                     rotation = {0, 0, 0},
                     scale = {1, 1, 1}                   
                 }
@@ -93,14 +146,45 @@ Entities={
             {
                 name = "MeshRenderComponent",
                 parameters = {                  
-                    isstatic = true,
-                    modelname = "enemigo.mesh",
-                    entityname= "sinbad",
-                    materialname = ""
+                    isstatic = false,
+                    modelname = "Sinbad.mesh",
+                    entityname= "sinbad5",
+                    materialname = "Sinbad/Gold"
                 }
-            }     
+            },
+            {
+                name = "ColliderComponent",
+                parameters = {
+                }
+            },
+            {
+                name = "RigidBodyComponent",
+                parameters = {
+                    shapeType = 1,
+                    movementType = 2,
+                    size = {2, 2, 2},
+                    mass = 1,
+                    friction =1,
+                    restitution=1,
+                    trigger = false,
+                    mask = 1,
+                    group = 2
+                }
+            },
+            {
+                name="NodeComponent",
+                parameters={
+                    ispatrol=true,
+                    cost=1.8,
+                    hcost=1.8,
+                    iswalkable=true,
+                    id=3
+
+                }
+
+            },
         }
-    }, 
+    },
     {
         name = "World",
         components = {
@@ -108,7 +192,7 @@ Entities={
                 name = "TransformComponent",
                 parameters = {  
                     a = 0,
-                    position = {0, 0, 0},
+                    position = {0, -30, 0},
                     rotation = {0, 0, 0},
                     scale = {1, 1, 1}                   
                 }
@@ -123,9 +207,18 @@ Entities={
                 parameters = {    
                     shapeType = SHAPES_BOX,
                     movementType = MOVEMENT_TYPE_STATIC,
-                    size = {300, 10, 300},
+                    size = {100, 1, 100},
                     mask = 1,
                     group = 1
+                }
+            },
+            {
+                name = "MeshRenderComponent",
+                parameters = {                  
+                    isstatic = false,
+                    modelname = "mapa1.mesh",
+                    entityname= "Mundo",
+                    materialname = "mapa1"
                 }
             }
         }
