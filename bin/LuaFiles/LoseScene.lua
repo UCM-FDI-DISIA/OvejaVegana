b804b12982761cbb3b59bd7d2d20658717763106
AgusCDT <acastrod@ucm.es>
2024-05-09 22:45:17 +0200

    button out

--- bin/LuaFiles/LoseScene.lua
+++ bin/LuaFiles/LoseScene.lua
@@ -1,43 +1,5 @@
 Entities={
     {
-        name = "MenuButton",
-        components = {           
-            {
-                name = "UITransformComponent",
-                parameters = {                  
-                    position = {0.4, 0.5},
-                    scale = {0.2, 0.1},
-                    hidden = false     
-                }
-            },
-            -- {        
-            --     name = "UISpriteRendererComponent",
-            --     parameters = {
-            --         name = "menu2", 
-            --         material = "menu", 
-            --         zOrder = 1
-            --     }
-            -- },
-            {
-                name = "UITextComponent",
-                parameters = {                  
-                    name = "menubutton",
-                    font = "Mario",
-                    caption = "MENU",
-                    zOrder = 3,
-                    charHeight = 0.1,
-                    color = {1.0,0.0,0.0} 
-                }
-            },
-            {
-                name = "MenuButtonComponent",
-                parameters = {  
-                                  
-                }
-            }
-        }
-    },
-    {
         name = "ExitButton",
         components = {           
             {
