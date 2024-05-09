cb17e980f2a6728ab3fac4d2d4a85f3dbad82ca1
AgusCDT <acastrod@ucm.es>
2024-05-09 03:21:51 +0200

    Desactivo camera Win/Lose-Menu

--- src/Components/MenuButtonComponent.cpp
+++ src/Components/MenuButtonComponent.cpp
@@ -1,7 +1,12 @@
 #include "MenuButtonComponent.h"
 #include "GameManager.h"
+#include "CameraComponent.h"
+#include "Entity.h"
+#include "Scene.h"
 
 void OvejaVegana::MenuButtonComponent::Action()
 {
+	my_cam = VeryReal::SceneManager::Instance()->GetActiveScene()->GetEntity("Camera")->GetComponent<VeryReal::CameraComponent>();
+	my_cam->desactive();
 	OvejaVegana::GameManager::Instance()->Menu();
 }
