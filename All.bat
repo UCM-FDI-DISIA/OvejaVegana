1ebaa565706feef06dcaf46ff7eaef6f87cd88f9
sheilajulvez <100275159+sheilajulvez@users.noreply.github.com>
2024-05-09 13:13:52 +0200

    Update All.bat
    Co-Authored-By: CristinaMora <100274708+CristinaMora@users.noreply.github.com>
    Co-Authored-By: josepk01 <72452701+josepk01@users.noreply.github.com>
    Co-Authored-By: CiscoGalvan <100274715+CiscoGalvan@users.noreply.github.com>

--- All.bat
+++ All.bat
@@ -10,3 +10,9 @@ xcopy bin .\..\..\bin\ /s /e
 cd ..\..\
 msbuild "OvejaVeganaSolucion.sln" /p:configuration=Debug /p:Platform=x64 /p:PlatformToolset=v143
 msbuild "OvejaVeganaSolucion.sln" /p:configuration=Release /p:Platform=x64 /p:PlatformToolset=v143
+
+
+cd .\bin
+
+ren  VeryRealProyecto_Release*.exe ______OvejaVegana_______*.exe
+start .
