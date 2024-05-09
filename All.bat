272e5a896d74ec8dc3f1b49f6be84686640708d1
AgusCDT <acastrod@ucm.es>
2024-05-10 00:39:32 +0200

    Update All.bat

--- All.bat
+++ All.bat
@@ -1,14 +1,12 @@
 ::compilamos el motor
 cd VeryReal\VeryRealSolucion\dependencies\cMake
 call All.bat
-cd VeryReal\VeryRealSolucion
-::copiamos la carpeta entera de bin a bin de juego
-xcopy bin .\..\..\bin\ /s /e
-
-
 cd ..\..\
 msbuild "OvejaVeganaSolucion.sln" /p:configuration=Debug /p:Platform=x64 /p:PlatformToolset=v143
 msbuild "OvejaVeganaSolucion.sln" /p:configuration=Release /p:Platform=x64 /p:PlatformToolset=v143
+cd VeryReal\VeryRealSolucion
+::copiamos la carpeta entera de bin a bin de juego
+xcopy bin .\..\..\bin\ /s /e
 
 
 cd .\bin
